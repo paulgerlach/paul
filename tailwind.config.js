3bcd268e83f532d68fbb3ce2c3e2783a5a4f28ea
Max Kobzar <nimbus2219@gmail.com>
2024-04-08 23:51:44 +0200

    added content

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./*.html', './js/main.js'],
+  theme: {
+    extend: {
+      borderRadius: {
+        base: '10px'
+      },
+    },
+  },
+  plugins: [],
+}
+

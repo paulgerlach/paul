1327ac43563c507905fccc6f5ed1c7e625f643d7
Maksym Kobzar <maksym.kobzar@vao.pl>
2024-04-22 14:36:25 +0200

    save

--- js/main.js
+++ js/main.js
@@ -46,34 +46,50 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   });
 
-  const s2 = new Swiper(".news-swiper", {
-    // Optional parameters
-    loop: true,
-    slidesPerView: 1,
-    centeredSlides: false,
-    mousewheel: true,
-    spaceBetween: 25,
+  // // Function to destroy Swiper instance
+  // function destroySwiper() {
+  //   s2.destroy(true, true); // Destroy Swiper instance with cleanup
+  // }
 
-    // If we need pagination
-    pagination: {
-      el: ".news-swiper-pagination",
-      clickable: true,
-      renderBullet: function (index, className) {
-        // Calculate which bullets to show
-        return '<span class="' + className + '">' + "</span>";
-      },
-    },
-  });
-
-  // Function to destroy Swiper instance
-  function destroySwiper() {
-    s2.destroy(true, true); // Destroy Swiper instance with cleanup
-  }
-
-  // Check window width on resize
-  window.addEventListener("resize", function () {
-    if (window.innerWidth >= 768) {
-      destroySwiper(); // Destroy Swiper if window width is >= 768px
-    }
-  });
+  // // Check window width on resize
+  // window.addEventListener("resize", function () {
+  //   if (window.innerWidth >= 768) {
+  //     destroySwiper(); // Destroy Swiper if window width is >= 768px
+  //   }
+  // });
 });
+
+var init = false;
+var swiper;
+function swiperCard() {
+  if (window.innerWidth <= 768) {
+    if (!init) {
+      init = true;
+      swiper = new Swiper(".brands-swiper", {
+        // Optional parameters
+        // loop: true,
+        slidesPerView: 1,
+        centeredSlides: true,
+        spaceBetween: 75,
+        mousewheel: true,
+
+        // If we need pagination
+        pagination: {
+          el: ".brand-swiper-pagination",
+          clickable: true,
+          renderBullet: function (index, className) {
+            // Calculate which bullets to show
+
+            return '<span class="' + className + '">' + "</span>";
+          },
+          dynamicBullets: true,
+        },
+      });
+    }
+  } else if (init) {
+    swiper.destroy();
+    init = false;
+  }
+}
+swiperCard();
+window.addEventListener("resize", swiperCard);

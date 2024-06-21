1727f53ffb5825fd6a65ffdbb7d3189c99c6599d
Maksym Kobzar <maksym.kobzar@vao.pl>
2024-06-21 15:37:42 +0200

    questionare updated

--- js/questionare.js
+++ js/questionare.js
@@ -3,201 +3,225 @@ const header = document.querySelector("#header");
 document.addEventListener("scroll", animateNavbarOnScroll);
 
 function animateNavbarOnScroll() {
-    let totalHeight = header.clientHeight + 250;
-    if (window.scrollY >= totalHeight) {
-        header.classList.add("scrolled");
-    } else {
-        header.classList.remove("scrolled");
-    }
+  let totalHeight = header.clientHeight + 250;
+  if (window.scrollY >= totalHeight) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
 }
 
 /* SLIDE UP */
 let slideUp = (target, duration = 300) => {
-    target.style.transitionProperty = "height, margin, padding";
-    target.style.transitionDuration = duration + "ms";
-    target.style.boxSizing = "border-box";
-    target.style.height = target.offsetHeight + "px";
-    target.offsetHeight;
-    target.style.overflow = "hidden";
-    target.style.height = 0;
-    target.style.paddingTop = 0;
-    target.style.paddingBottom = 0;
-    target.style.marginTop = 0;
-    target.style.marginBottom = 0;
-    target.style.border = "none";
+  target.style.transitionProperty = "height, margin, padding";
+  target.style.transitionDuration = duration + "ms";
+  target.style.boxSizing = "border-box";
+  target.style.height = target.offsetHeight + "px";
+  target.offsetHeight;
+  target.style.overflow = "hidden";
+  target.style.height = 0;
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  target.style.border = "none";
 
-    window.setTimeout(() => {
-        target.style.display = "none";
-        target.style.removeProperty("height");
-        target.style.removeProperty("padding-top");
-        target.style.removeProperty("padding-bottom");
-        target.style.removeProperty("margin-top");
-        target.style.removeProperty("margin-bottom");
-        target.style.removeProperty("overflow");
-        target.style.removeProperty("transition-duration");
-        target.style.removeProperty("transition-property");
-        target.style.removeProperty("border");
-    }, duration);
-};
-/* SLIDE DOWN */
-let slideDown = (target, duration = 300) => {
-    target.style.removeProperty("display");
-    let display = window.getComputedStyle(target).display;
-    if (display === "none") display = "grid";
-    target.style.display = display;
-    let height = target.offsetHeight;
-    target.style.overflow = "hidden";
-    target.style.height = 0;
-    target.style.paddingTop = 0;
-    target.style.paddingBottom = 0;
-    target.style.marginTop = 0;
-    target.style.marginBottom = 0;
-    target.offsetHeight;
-    target.style.boxSizing = "border-box";
-    target.style.transitionProperty = "height, margin, padding";
-    target.style.transitionDuration = duration + "ms";
-    target.style.height = height + "px";
-    target.style.border = "none";
-
+  window.setTimeout(() => {
+    target.style.display = "none";
+    target.style.removeProperty("height");
     target.style.removeProperty("padding-top");
     target.style.removeProperty("padding-bottom");
     target.style.removeProperty("margin-top");
     target.style.removeProperty("margin-bottom");
+    target.style.removeProperty("overflow");
+    target.style.removeProperty("transition-duration");
+    target.style.removeProperty("transition-property");
     target.style.removeProperty("border");
+  }, duration);
+};
+/* SLIDE DOWN */
+let slideDown = (target, duration = 300) => {
+  target.style.removeProperty("display");
+  let display = window.getComputedStyle(target).display;
+  if (display === "none") display = "grid";
+  target.style.display = display;
+  let height = target.offsetHeight;
+  target.style.overflow = "hidden";
+  target.style.height = 0;
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  target.offsetHeight;
+  target.style.boxSizing = "border-box";
+  target.style.transitionProperty = "height, margin, padding";
+  target.style.transitionDuration = duration + "ms";
+  target.style.height = height + "px";
+  target.style.border = "none";
 
-    window.setTimeout(() => {
-        target.style.removeProperty("height");
-        target.style.removeProperty("overflow");
-        target.style.removeProperty("transition-duration");
-        target.style.removeProperty("transition-property");
-        target.style.removeProperty("border");
-    }, duration);
+  target.style.removeProperty("padding-top");
+  target.style.removeProperty("padding-bottom");
+  target.style.removeProperty("margin-top");
+  target.style.removeProperty("margin-bottom");
+  target.style.removeProperty("border");
+
+  window.setTimeout(() => {
+    target.style.removeProperty("height");
+    target.style.removeProperty("overflow");
+    target.style.removeProperty("transition-duration");
+    target.style.removeProperty("transition-property");
+    target.style.removeProperty("border");
+  }, duration);
 };
 
 const questionareInfo = document.querySelector(".questionare-info");
 const questionareInfoHeader = questionareInfo.querySelector(
-    ".questionare-answer-header"
+  ".questionare-answer-header"
 );
 const questionareInfoContent = questionareInfo.querySelector(
-    ".questionare-answer-content"
+  ".questionare-answer-content"
 );
 
 questionareInfoHeader.addEventListener("click", () => {
-    if (questionareInfoHeader.classList.contains("opened")) {
-        questionareInfoHeader.classList.remove("opened");
-        slideDown(questionareInfoContent);
-    } else {
-        questionareInfoHeader.classList.add("opened");
-        slideUp(questionareInfoContent);
-    }
+  if (questionareInfoHeader.classList.contains("opened")) {
+    questionareInfoHeader.classList.remove("opened");
+    slideDown(questionareInfoContent);
+  } else {
+    questionareInfoHeader.classList.add("opened");
+    slideUp(questionareInfoContent);
+  }
 });
 
-let currentStep = '1';
-const totalSteps = '7';
+let currentStep = "1";
+const totalSteps = "7";
 
-const nextStepButton = document.querySelector('#next-step');
-const prevStepButton = document.querySelector('#prev-step');
-const skipStepButton = document.querySelector('#skip-step');
+const nextStepButton = document.querySelector("#next-step");
+const prevStepButton = document.querySelector("#prev-step");
+const skipStepButton = document.querySelector("#skip-step");
 
 function nextStep() {
-    if (currentStep.toString() === totalSteps.toString()) {
-        document.querySelector('.questionare-steps').classList.add('hidden');
-        document.getElementById('questionare-final').classList.remove('hidden');
-        document.querySelector('.steps-progress').classList.add('hidden');
-        skipStepButton.classList.add('hidden');
+  if (Number(currentStep) === Number(totalSteps)) {
+    document.querySelector(".questionare-steps").classList.add("hidden");
+    document.getElementById("questionare-final").classList.remove("hidden");
+    document.querySelector(".steps-progress").classList.add("hidden");
+  }
+  if (currentStep < totalSteps) {
+    // Hide current step
+    const currentStepElement = document.querySelector(
+      `[data-step="${currentStep}"]`
+    );
+    currentStepElement.classList.add("hidden");
+    document.querySelector(".steps-progress").classList.remove("hidden");
+
+    // Show next step
+    currentStep++;
+    const nextStepElement = document.querySelector(
+      `[data-step="${currentStep}"]`
+    );
+    if (Number(currentStep) === Number(totalSteps)) {
+      skipStepButton.classList.add("hidden");
+    } else {
+      skipStepButton.classList.remove("hidden");
     }
-    if (currentStep < totalSteps) {
-        // Hide current step
-        const currentStepElement = document.querySelector(`[data-step="${currentStep}"]`);
-        currentStepElement.classList.add('hidden');
-        document.querySelector('.steps-progress').classList.remove('hidden');
+    nextStepElement.classList.remove("hidden");
+    prevStepButton.classList.remove("hidden");
 
-        // Show next step
-        currentStep++;
-        const nextStepElement = document.querySelector(`[data-step="${currentStep}"]`);
-        nextStepElement.classList.remove('hidden');
-        prevStepButton.classList.remove('hidden');
-        skipStepButton.classList.remove('hidden');
+    // Update progress bar
+    const progressStepElement = document.querySelector(
+      `[data-step-index="${currentStep - 1}"]`
+    );
+    progressStepElement.classList.remove("bg-dark_green/10");
+    progressStepElement.classList.add("bg-green");
+    slideUp(questionareInfoContent);
 
-        // Update progress bar
-        const progressStepElement = document.querySelector(`[data-step-index="${currentStep - 1}"]`);
-        progressStepElement.classList.remove('bg-dark_green/10');
-        progressStepElement.classList.add('bg-green');
-        slideUp(questionareInfoContent);
-
-        // Update progress bar attribute
-        document.querySelector('.steps-progress').setAttribute('data-filled-steps', currentStep - 1);
-    }
+    // Update progress bar attribute
+    document
+      .querySelector(".steps-progress")
+      .setAttribute("data-filled-steps", currentStep - 1);
+  }
 }
 
 function prevStep() {
-    if (currentStep > 1) {
-        // Hide current step
-        const currentStepElement = document.querySelector(`[data-step="${currentStep}"]`);
-        currentStepElement.classList.add('hidden');
+  if (currentStep > 1) {
+    // Hide current step
+    const currentStepElement = document.querySelector(
+      `[data-step="${currentStep}"]`
+    );
+    currentStepElement.classList.add("hidden");
 
-        // Show previous step
-        currentStep--;
-        const prevStepElement = document.querySelector(`[data-step="${currentStep}"]`);
-        prevStepElement.classList.remove('hidden');
-
-        // Update progress bar
-        const progressStepElement = document.querySelector(`[data-step-index="${currentStep}"]`);
-        progressStepElement.classList.add('bg-dark_green/10');
-        progressStepElement.classList.remove('bg-green');
-
-        // Update progress bar attribute
-        document.querySelector('.steps-progress').setAttribute('data-filled-steps', currentStep - 1);
+    // Show previous step
+    currentStep--;
+    if (Number(currentStep) === Number(totalSteps)) {
+      skipStepButton.classList.add("hidden");
+    } else {
+      skipStepButton.classList.remove("hidden");
     }
+    const prevStepElement = document.querySelector(
+      `[data-step="${currentStep}"]`
+    );
+    prevStepElement.classList.remove("hidden");
+
+    // Update progress bar
+    const progressStepElement = document.querySelector(
+      `[data-step-index="${currentStep}"]`
+    );
+    progressStepElement.classList.add("bg-dark_green/10");
+    progressStepElement.classList.remove("bg-green");
+
+    // Update progress bar attribute
+    document
+      .querySelector(".steps-progress")
+      .setAttribute("data-filled-steps", currentStep - 1);
+  }
 }
 
-nextStepButton.addEventListener('click', () => nextStep());
-skipStepButton.addEventListener('click', () => nextStep());
-prevStepButton.addEventListener('click', () => prevStep());
+nextStepButton.addEventListener("click", () => nextStep());
+skipStepButton.addEventListener("click", () => nextStep());
+prevStepButton.addEventListener("click", () => prevStep());
 
-const questForm = document.querySelector('#questionare-form');
+const questForm = document.querySelector("#questionare-form");
 const questRadioInputs = questForm.querySelectorAll("input[type='radio']");
 const questSelect = questForm.querySelector("select");
 
 questRadioInputs.forEach((radioInput) => {
-    radioInput.addEventListener('change', () => {
-        setTimeout(() => {
-            nextStep();
-        }, 300)
-    })
-})
-
-questSelect.addEventListener('change', () => {
+  radioInput.addEventListener("change", () => {
     setTimeout(() => {
-        nextStep();
-    }, 300)
-})
+      nextStep();
+    }, 300);
+  });
+});
 
-document.addEventListener("DOMContentLoaded", function() {
-    const customSelectWrapper = document.querySelector(".custom-select-wrapper");
-    const customSelect = document.querySelector(".custom-select");
-    const customSelectTrigger = document.querySelector(".custom-select-trigger");
-    const customOptions = document.querySelector(".custom-options");
-    const customOptionsItems = document.querySelectorAll(".custom-option");
-    const selectElement = document.querySelector("#form_select");
+questSelect.addEventListener("change", () => {
+  setTimeout(() => {
+    nextStep();
+  }, 300);
+});
 
-    customSelectTrigger.addEventListener("click", function() {
-        customOptions.classList.toggle("open");
+document.addEventListener("DOMContentLoaded", function () {
+  const customSelectWrapper = document.querySelector(".custom-select-wrapper");
+  const customSelect = document.querySelector(".custom-select");
+  const customSelectTrigger = document.querySelector(".custom-select-trigger");
+  const customOptions = document.querySelector(".custom-options");
+  const customOptionsItems = document.querySelectorAll(".custom-option");
+  const selectElement = document.querySelector("#form_select");
+
+  customSelectTrigger.addEventListener("click", function () {
+    customOptions.classList.toggle("open");
+  });
+
+  customOptionsItems.forEach((option) => {
+    option.addEventListener("click", function () {
+      customOptionsItems.forEach((item) => item.classList.remove("selected"));
+      this.classList.add("selected");
+      customSelectTrigger.querySelector("span").textContent = this.textContent;
+      selectElement.value = this.getAttribute("data-value");
+      customOptions.classList.remove("open");
     });
+  });
 
-    customOptionsItems.forEach(option => {
-        option.addEventListener("click", function() {
-            customOptionsItems.forEach(item => item.classList.remove("selected"));
-            this.classList.add("selected");
-            customSelectTrigger.querySelector("span").textContent = this.textContent;
-            selectElement.value = this.getAttribute("data-value");
-            customOptions.classList.remove("open");
-        });
-    });
-
-    document.addEventListener("click", function(event) {
-        if (!customSelect.contains(event.target)) {
-            customOptions.classList.remove("open");
-        }
-    });
+  document.addEventListener("click", function (event) {
+    if (!customSelect.contains(event.target)) {
+      customOptions.classList.remove("open");
+    }
+  });
 });
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
 // vite.config.js
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                gerate: resolve(__dirname, 'gerate.html'),
-                kontakt: resolve(__dirname, 'kontakt.html'),
-                datenschutzhinweise: resolve(__dirname, 'datenschutzhinweise.html'),
-                funktionen: resolve(__dirname, 'funktionen.html'),
-                impressum: resolve(__dirname, 'impressum.html'),
-                preise: resolve(__dirname, 'preise.html'),
-                fragebogen: resolve(__dirname, 'fragebogen.html'),
-                regulatorik: resolve(__dirname, 'regulatorik.html'),
-            },
-        },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        gerate: resolve(__dirname, "gerate.html"),
+        kontakt: resolve(__dirname, "kontakt.html"),
+        datenschutzhinweise: resolve(__dirname, "datenschutzhinweise.html"),
+        funktionen: resolve(__dirname, "funktionen.html"),
+        impressum: resolve(__dirname, "impressum.html"),
+        preise: resolve(__dirname, "preise.html"),
+        fragebogen: resolve(__dirname, "fragebogen.html"),
+        regulatorik: resolve(__dirname, "./blog/regulatorik.html"),
+      },
     },
+  },
 });

b000b6fe0fd7b067002362573474b88a05f4ca32
Max Kobzar <nimbus2219@gmail.com>
2025-05-19 14:28:50 +0100

    config drizzle

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,6 @@
+import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-
-const queryClient = postgres(process.env.DATABASE_URL!);
-const db = drizzle(queryClient);
+config({ path: ".env" });
+const client = postgres(process.env.DATABASE_URL!);
+export const db = drizzle({ client });

cec200e48560885d407715f5948f831cb04c3915
Nolo Cloud Solutions <thulomaepa@gmail.com>
2025-12-15 19:27:07 +0200

    style icon

--- src/components/ChatBot/index.tsx
+++ src/components/ChatBot/index.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { MessageCircle, Sparkles } from "lucide-react";
 
 export default function ChatBot() {
   return (
-    <div>ChatBot</div>
-  )
+    <div className='bg-green cursor-pointer hover:scale-105 transition ease-in-out rounded-full shadow-md p-3'>
+      <MessageCircle className="w-12 h-12" color='#FFFFFF'/>
+    </div>
+  );
 }

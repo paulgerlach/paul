c91dd6856023eeb57ce8be963f3c0933a7580474
Nolo Cloud Solutions <thulomaepa@gmail.com>
2025-12-17 17:58:09 +0200

    add styling to slack chat

--- src/components/Admin/SlackbotChatContainer/ChatContainer.tsx
+++ src/components/Admin/SlackbotChatContainer/ChatContainer.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+'use client'
 
-export default function ChatContainer() {
+import ChatHeader from '@/components/Common/ChatBot/ChatHeader';
+import React from 'react'
+import { FaRegWindowMinimize } from 'react-icons/fa';
+import { SiChatbot } from 'react-icons/si';
+
+interface ChatContainerProps {
+  setShowSlackChat: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function ChatContainer({ setShowSlackChat }: ChatContainerProps) {
+  
+  const handleMinimizeChat = () => {
+    setShowSlackChat(false);
+  };
   return (
-    <div>ChatContainer</div>
-  )
+    <div className="flex flex-col bg-slate-100 p-4 rounded-md shadow-lg h-[100vh] max-w-full relative animate-from-right">
+      <FaRegWindowMinimize
+        onClick={handleMinimizeChat}
+        className="self-end cursor-pointer hover:-translate-y-1 transition ease-in-out absolute"
+      />
+      {/* Header */}
+      <ChatHeader headerText='Text Support' subHeaderText='Chat Assistant'/>
+
+      <p>ChatContainer</p>
+    </div>
+  );
 }
--- src/components/Admin/SlackbotChatContainer/index.tsx
+++ src/components/Admin/SlackbotChatContainer/index.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import React, { useEffect, useRef, useState } from 'react';
-import { MessageSquareQuote } from "lucide-react";
 import ChatContainer from './ChatContainer';
+import { PiChatCircleDotsFill } from 'react-icons/pi';
+import './ChatContainer.css'
 
 export default function SlackBotChatContainer() {
   const [showSlackChat, setShowSlackChat] = useState(false);
-    const chatContainerRef = useRef<HTMLDivElement>(null);
+  const chatContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -31,18 +32,19 @@ export default function SlackBotChatContainer() {
     setShowSlackChat((prev) => !prev);
   };
   return (
-    <div className="fixed bottom-8 right-8 z-10">
-      {showSlackChat && (
+    <div className="fixed bottom-8 right-8  z-[999]">
+      {showSlackChat ? (
         <div ref={chatContainerRef} className="chat-window">
-          <ChatContainer />
+          <ChatContainer setShowSlackChat={setShowSlackChat} />
         </div>
+      ) : (
+        <PiChatCircleDotsFill
+          onClick={toggleChat}
+          className="w-auto h-auto bg-black cursor-pointer hover:scale-105 transition ease-in-out rounded-full shadow-md p-4"
+          color="#FFFFFF"
+          size={28}
+        />
       )}
-      <MessageSquareQuote
-        onClick={toggleChat}
-        className="w-auto h-auto bg-green cursor-pointer hover:scale-105 transition ease-in-out rounded-full shadow-md p-3"
-        color="#757575"
-        size={40}
-      />
     </div>
   );
 }

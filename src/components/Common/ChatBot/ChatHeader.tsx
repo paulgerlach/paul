896b81a520fe6e2627e8a64481fba99fbd16ddef
Nolo Cloud Solutions <thulomaepa@gmail.com>
2025-12-21 15:34:53 +0200

    adjust header font size

--- src/components/Common/ChatBot/ChatHeader.tsx
+++ src/components/Common/ChatBot/ChatHeader.tsx
@@ -13,12 +13,16 @@ export default function ChatHeader({ headerText, subHeaderText }: HeaderProps) {
       <div className="flex flex-row gap-1 items-center justify-center animate-from-left shadow-md w-auto px-4 py-2 rounded-full bg-white">
         <div className="relative">
           <div className="bg-green-700 rounded-full w-4 h-4 absolute right-0" />
-          <Image alt="chat avatar"
-src={max_chat_avatar.src}
-width={40} height={40} className="rounded-full" />
+          <Image
+            alt="chat avatar"
+            src={max_chat_avatar.src}
+            width={40}
+            height={40}
+            className="rounded-full"
+          />
         </div>
-        <div className="text-center text-lg font-semibold">
-          <p className="text-md">{headerText}</p>
+        <div className="text-center text-lg ">
+          <p className="text-sm font-semibold">{headerText}</p>
           <p className="text-xs text-gray-400">{subHeaderText}</p>
         </div>
       </div>

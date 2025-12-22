3f4dd58843b572ed5ecf40172fe04dd9ede6a931
Nolo Cloud Solutions <thulomaepa@gmail.com>
2025-12-22 09:11:20 +0200

    move green dot to top right

--- src/components/Common/ChatBot/ChatHeader.tsx
+++ src/components/Common/ChatBot/ChatHeader.tsx
@@ -10,9 +10,9 @@ interface HeaderProps {
 export default function ChatHeader({ headerText, subHeaderText }: HeaderProps) {
   return (
     <div className="pb-4 flex justify-center">
-      <div className="flex flex-row gap-1 items-center justify-center animate-from-left shadow-md w-auto px-4 py-2 rounded-full bg-white">
+      <div className="flex flex-row gap-2 items-center justify-center animate-from-left shadow-md w-auto px-4 py-2 rounded-full bg-white">
         <div className="relative">
-          <div className="bg-green-700 rounded-full w-4 h-4 absolute right-0" />
+          <div className="bg-green-700 rounded-full w-4 h-4 absolute -right-1 -top-1" />
           <Image
             alt="chat avatar"
             src={max_chat_avatar.src}

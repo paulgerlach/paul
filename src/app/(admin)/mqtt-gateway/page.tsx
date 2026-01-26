559a7f86a7027548a9e242962f085ae6bb22d9e8
Nolo Cloud Solutions <thulomaepa@gmail.com>
2026-01-26 22:54:43 +0200

    add GatewayContent component

--- src/app/(admin)/mqtt-gateway/page.tsx
+++ src/app/(admin)/mqtt-gateway/page.tsx
@@ -1,19 +1,11 @@
-import Breadcrumb from '@/components/Admin/Breadcrumb/Breadcrumb';
-import ContentWrapper from '@/components/Admin/ContentWrapper/ContentWrapper';
-import { ROUTE_DASHBOARD } from '@/routes/routes';
-import React from 'react'
+import Loading from "@/components/Basic/Loading/Loading";
+import React, { Suspense } from "react";
+import GatewayContent from "./GatewayContent";
 
 export default function MQTTGatewayManagement() {
   return (
-    <div className="py-6 px-9 max-medium:px-4 max-medium:py-4 h-[calc(100dvh-77px)] max-h-[calc(100dvh-77px)] max-xl:h-[calc(100dvh-53px)] max-xl:max-h-[calc(100dvh-53px)] max-medium:h-auto max-medium:max-h-none max-medium:overflow-y-auto grid grid-rows-[auto_1fr]">
-      <Breadcrumb
-        backTitle="Abrechnung"
-        link={ROUTE_DASHBOARD}
-        title="Gateway Management"
-      />
-      <ContentWrapper className="gap-14 max-medium:gap-6 grid grid-cols-2 max-medium:grid-cols-1">
-        <p>MQTTGatewayManagement</p>
-      </ContentWrapper>
-    </div>
+    <Suspense fallback={<Loading/>}>
+      <GatewayContent />;
+    </Suspense>
   );
 }

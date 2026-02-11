bba209ea077e590c35cadd85aac50e49819cebb3
Nolo Cloud Solutions <thulomaepa@gmail.com>
2026-02-11 15:52:17 +0200

    Fix build

--- src/components/Header/AdminHeader/AdminAccountDropdown/ModalFooter.tsx
+++ src/components/Header/AdminHeader/AdminAccountDropdown/ModalFooter.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-export default function ModalFooter({ onClose, loading }: { onClose: () => void, loading?: boolean }) {
+export default function ModalFooter({ onClose, loading, onSave }: { onClose: () => void, loading?: boolean, onSave?: () => void   }) {
   return (
     <div className="flex justify-between items-center pt-6 mt-2">
       <button
@@ -11,14 +11,16 @@ export default function ModalFooter({ onClose, loading }: { onClose: () => void,
         Abbrechen
       </button>
 
-
+      {onSave &&
       <button
-        type="submit"
+        type="button"
+        onClick={onSave}
         disabled={loading}
         className="px-6 py-2.5 text-sm font-medium text-white bg-[#7AD085] rounded-md hover:bg-[#6bc176] shadow-sm transition-colors disabled:opacity-50"
       >
         {loading ? "Lädt..." : "Speichern"}
       </button>
+      }
     </div>
   );
 }

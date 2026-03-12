643d07f3a2a76146dc40d51ac93b48e2f8a92b4d
Nolo Cloud Solutions <thulomaepa@gmail.com>
2026-03-12 13:52:16 +0200

    use defined table update type

--- src/actions/admin/assignPropertyToAgency.ts
+++ src/actions/admin/assignPropertyToAgency.ts
@@ -1,13 +1,16 @@
 'use server'
 
+import { TablesUpdate } from "@/types/supabase";
 import { supabaseServer } from "@/utils/supabase/server";
 
 // src/actions/admin/assignPropertyToAgency.ts
 export async function assignPropertyToAgency(propertyId: string, agencyId: string) {
   const supabase = await supabaseServer();
 
+  const update: TablesUpdate<"objekte"> = { agency_id: agencyId };
+
   await supabase
     .from("objekte")
-    .update({ agency_id: agencyId } )
+    .update(update)
     .eq("id", propertyId);
 }
